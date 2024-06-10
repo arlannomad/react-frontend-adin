6aa9430210eb48dac33bb95e7f69ce3b273448a8
arlan <arlannomad@gmail.com>
2024-06-10 13:17:58 +0500

    react-frontend-adin

--- src/Components/Campus/Campus.jsx
+++ src/Components/Campus/Campus.jsx
@@ -5,16 +5,17 @@ import gallery_2 from '../../assets/gallery-2.png'
 import gallery_3 from '../../assets/gallery-3.png'
 import gallery_4 from '../../assets/gallery-4.png'
 import white_arrow from '../../assets/white-arrow.png'
-import brothers from '../../assets/brothers.jpg'
+// import brothers from '../../assets/brothers.jpg'
+import gazoblok_almaty from '../../assets/gazoblok-almaty-foto.jpg'
 
 const Campus = () => {
   return (
     <div className='campus'>
       <div className="gallery">
-          <img src={brothers} alt="" />
-          <img src={brothers} alt="" />
-          <img src={brothers} alt="" />
-          <img src={brothers} alt="" />
+          <img src={gazoblok_almaty} alt="" />
+          <img src={gazoblok_almaty} alt="" />
+          <img src={gazoblok_almaty} alt="" />
+          <img src={gazoblok_almaty} alt="" />
       </div>
       {/* <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button> */}
     </div>

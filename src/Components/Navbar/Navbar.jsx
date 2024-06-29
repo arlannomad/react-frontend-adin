7a78cf9dd98c551072500824253a157e7f7db660
arlan <arlannomad@gmail.com>
2024-06-29 11:14:31 +0500

    Added  <li><a href="https://adin-fontend-list.netlify.app/">

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -24,13 +24,16 @@ const Navbar = () => {
         <nav className={`container ${sticky? 'dark-nav' : ''}`}>
             <img src={photologo} alt="" className='logo'/>
             <img src="" alt="" className='logo'/>
-            <ul className={mobileMenu?'':'hide-mobile-menu'}>
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
                 <li><Link to='hero' smooth={true} offset={0} duration={500}>Главная</Link></li>
                 <li><Link to='program' smooth={true} offset={-260} duration={500}>Что мы предлагаем</Link></li>
                 <li><Link to='about' smooth={true} offset={-150} duration={500}>О нас</Link></li>
                 <li><Link to='campus' smooth={true} offset={-260} duration={500}>Наши продукты</Link></li>
                 <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Отзывы клиентов</Link></li>
-                <li><a href="http://localhost:8081/"><button className='btn'>Статьи</button></a></li>
+                {/*<li><a href="http://localhost:8081/"><button className='btn'>Статьи</button></a></li>*/}
+                <li><a href="https://adin-fontend-list.netlify.app/">
+                    <button className='btn'>Статьи</button>
+                </a></li>
                 <li><Link Link to='contact' smooth={true} offset={-260} duration={500}
                           className='btn'>Контакты</Link></li>
             </ul>
